3e11c7168242c75be6b898ae34038041844c8db8
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2023-02-18 20:52:27 +0000

    Added smooth camera following and terrain for testing in level

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+    public Vector3 positionOffset;
+    public Vector3 angleOffset;
+    public bool cameraPreset = true;
+
+    private void Start()
+    {
+
+    }
+    void FixedUpdate ()
+    {
+        Vector3 exactfollow = target.position + positionOffset;
+        Vector3 smoothfollow = Vector3.Lerp(transform.position, exactfollow, smoothspeed);
+        transform.position = smoothfollow;
+        
+
+        //transform.LookAt(target.position + angleOffset);
+    }
+}
