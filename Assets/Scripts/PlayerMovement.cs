fe94aa8a3826d81239bf15213293d3919806bf7e
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2023-03-17 12:43:29 +0000

    Made swinging only rotate the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -50,26 +50,35 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Grounded)
+        if (swinging)
         {
-            movementOutputVector.y = -0.02f; // Keeps player stuck to ground
-        }
-        else if (swinging)
-        {
-            this.transform.RotateAround(pivotPosition, Vector3.forward, movementInputVector.ReadValue<Vector2>().x);
+            float rotateForce = movementInputVector.ReadValue<Vector2>().x;
+            if (rotateForce == 0)
+            {
+                rotateForce = this.transform.rotation.eulerAngles.x/20;
+            }
+            this.transform.RotateAround(pivotPosition, Vector3.forward, rotateForce);
             //this.transform.Rotate(new Vector3(0, 0, move.ReadValue<Vector2>().x));
         }
         else
         {
-            if (movementInputVector.ReadValue<Vector2>().y > 0 && jumpDuration<maxJump) // Times jump button for higher/lower jumps
+            if (Grounded)
             {
-                jumpDuration += Time.fixedDeltaTime;
+                movementOutputVector.y = -0.02f; // Keeps player stuck to ground
             }
             else
-                movementOutputVector.y += -gravity;
+            {
+                if (movementInputVector.ReadValue<Vector2>().y > 0 && jumpDuration < maxJump) // Times jump button for higher/lower jumps
+                {
+                    jumpDuration += Time.fixedDeltaTime;
+                }
+                else
+                    movementOutputVector.y += -gravity;
+            }
+            controller.Move(movementOutputVector); // Moves the player with values calculated above and in Update()
+
         }
         //Debug.Log(movementVector.x + ", " + movementVector.y);
-        controller.Move(movementOutputVector); // Moves the player with values calculated above and in Update()
         Grounded = controller.isGrounded;
     }
 
@@ -88,6 +97,7 @@ public class PlayerMovement : MonoBehaviour
                 movementOutputVector.y = jumpSpeed;
                 swinging = false;
                 Debug.Log("Jumped off swing: " + movementOutputVector.x + ", " + movementOutputVector.y);
+                ExitSwing();
             }            
         }
         if (xy.y>0)
@@ -96,41 +106,50 @@ public class PlayerMovement : MonoBehaviour
             {
                 // TODO: Check the player isn't hitting something above them to do the next part
                 movementOutputVector.y = jumpSpeed;
-                //controller.Move(movementVector);
-                Debug.Log("Jump!");
-                movementOutputVector.y = jumpSpeed;
                 jumpDuration = 0;
-                Debug.Log("Jump stopped. Movement is: "+movementOutputVector.x + ", " + movementOutputVector.y);
+                //Debug.Log("Jump stopped. Movement is: "+movementOutputVector.x + ", " + movementOutputVector.y);
                 Grounded = false;
             }            
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void ExitSwing()
     {
-        if (collision.gameObject.tag == "Swing")
+        this.transform.rotation = Quaternion.Euler(0, 90, 0);
+        swinging = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Swing")
         {
             Debug.Log("Entered swing radius");
-            this.transform.LookAt(collision.gameObject.transform);
-            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x +90, 90, 0);
-            pivotPosition = collision.gameObject.transform.position;
-            swinging = true;
+
+            if (other.transform.position.y > this.transform.position.y) // Only if swing is above the player (Can't use contacts with trigger)
+            {
+                this.transform.LookAt(other.gameObject.transform);
+                this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x + 90, 90, 0);
+                pivotPosition = other.gameObject.transform.position;
+                swinging = true;
+            }
         }
-        else if (!Grounded)
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!Grounded)
         {
             jumpDuration = maxJump; // When the player hits something above them, extending the jump doesn't work.
             movementOutputVector.y = -gravity;
             Debug.Log("Head hit by non-swing. Stopping jump.");
         }
     }
-
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.tag == "Swing")
+        if (other.gameObject.tag == "Swing")
         {
             Debug.Log("Exiting swing radius");
-            this.transform.rotation = Quaternion.Euler(0, 90, 0);
-            swinging = false;
+            ExitSwing();
         }
     }
 }
