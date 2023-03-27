a5290ac3de33b065b42619fa5a48093937c87948
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2023-03-27 15:01:44 +0100

    Can throw banana depending on the direction the player is facing.
    Example banana removed

--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -6,31 +6,17 @@ using UnityEngine.InputSystem;
 public class ThrowProjectile : MonoBehaviour
 {
     [SerializeField] private GameObject projectileObject;
-    public Vector3 projectileSpeed = new Vector3(3, 0, 0);
+    public float projectileSpeed = 3;
 
     public PlayerControls playerControls;
     private InputAction throwControl;
 
     private bool m_fire;
     public float fireCooldownTime = 1;
-    private float cooldown;
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-        throwControl = playerControls.Player.Fire;
-        throwControl.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-        throwControl.Disable();
-    }
+    private float cooldown = 0;
 
     private void Start()
     {
-        playerControls = new PlayerControls();
         cooldown = fireCooldownTime;
     }
 
@@ -45,13 +31,24 @@ public class ThrowProjectile : MonoBehaviour
     {
         if (cooldown>0)
             cooldown -= Time.deltaTime;
-        if (m_fire && cooldown<=0)
+        if (m_fire && cooldown <= 0)
         {
             cooldown = fireCooldownTime;
-            Debug.Log("Throw!");
             GameObject newProjectile = Object.Instantiate(projectileObject);
-            newProjectile.transform.position = new Vector3(this.transform.position.x+3, this.transform.position.y, this.transform.position.z);
-            newProjectile.GetComponent<Rigidbody>().velocity = projectileSpeed; // TODO: Change velocity dependant on the direction the monkey faces
+            Debug.Log("Rotation: " + this.transform.rotation.eulerAngles.y);
+
+            if (this.transform.rotation.eulerAngles.y < 180)
+            {
+                Debug.Log("Right throw");
+                newProjectile.transform.position = new Vector3(this.transform.position.x + 1, this.transform.position.y + 1, this.transform.position.z);
+                newProjectile.GetComponent<Rigidbody>().velocity = new Vector3(projectileSpeed, 3, 0); // TODO: Change velocity dependant on the direction the monkey faces
+            }
+            else
+            {
+                Debug.Log("Left throw");
+                newProjectile.transform.position = new Vector3(this.transform.position.x - 1, this.transform.position.y + 1, this.transform.position.z);
+                newProjectile.GetComponent<Rigidbody>().velocity = new Vector3(-projectileSpeed, 3, 0); ; // TODO: Change velocity dependant on the direction the monkey faces
+            }
         }
     }
 }
